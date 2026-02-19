50bf49cda3c3b094dd1b842293f006e59fc9d467
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-02-19 15:23:06 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,7 +13,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package zenity-gtk3
+make-aur-package zenity-rs-bin
 make-aur-package
 
 # If the application needs to be manually built that has to be done down here
