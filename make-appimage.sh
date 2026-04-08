8cb4d163b3fd14eb67407402c2fbbba7be3123c1
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-04-08 19:32:43 +0200

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,7 @@ VERSION=$(pacman -Q tachoparser | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 #export ADD_HOOKS="self-updater.bg.hook"
-#export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=DUMMY
 export DESKTOP=DUMMY
 export MAIN_BIN=dddui
